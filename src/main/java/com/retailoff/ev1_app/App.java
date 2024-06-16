2f0a7369022f04d2e81b0d653163aa2cb28d95bf
JonaZenteno <jon.zenteno@gmail.com>
2024-06-16 16:57:30 -0400

    commit inicial

---
+++ src/main/java/com/retailoff/ev1_app/App.java
@@ -0,0 +1,7 @@
+package com.retailoff.ev1_app;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Retail Off!");
+    }
+}
---
+++ src/test/java/com/retailoff/ev1_app/AppTest.java
@@ -0,0 +1,12 @@
+package com.retailoff.ev1_app;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
